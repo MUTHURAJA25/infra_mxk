3e6ba86cac2a4a6e85cf55f14f8b68181a0a62a9
MUTHURAJA25 <142464451+MUTHURAJA25@users.noreply.github.com>
2025-11-06 23:38:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-25"
-    key            = "infra/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "my-terraform-state-bucket-25"
+  #   key            = "infra/terraform.tfstate"
+  #   region         = "us-east-2"
+  #   encrypt        = true
+  # }
 }
