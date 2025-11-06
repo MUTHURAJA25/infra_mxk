4ca57bf2443ba7106b295b2d15d6a61a6b44849a
MUTHURAJA25 <142464451+MUTHURAJA25@users.noreply.github.com>
2025-11-06 11:36:59 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,15 +2,32 @@ variable "aws_region" {
   default = "ap-south-1"
 }
 
+variable "project_name" {
+  default = "jenkins-aws-demo"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
 variable "instance_type" {
   default = "t2.micro"
 }
 
 variable "key_name" {
-  description = "mxk_123"
   type        = string
+  description = "EC2 key pair name"
 }
 
-variable "project_name" {
-  default = "jenkins-aws-demo"
+variable "ami_id" {
+  description = "Ubuntu AMI for ap-south-1"
+  default     = "ami-04568bde0d686e039"
+}
+
+variable "az1" {
+  default = "ap-south-1a"
 }
